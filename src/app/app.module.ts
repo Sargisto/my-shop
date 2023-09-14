d5d10cb8c38ded9b06c2d3da7b2651928f725841
tikopapikian <tigran.papikyan@diatomenterprises.com>
2023-09-14 18:49:57 +0400

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { TableItemsComponent } from './table-items/table-items.component';
+import { AuthGuardAdmin } from './login/auth.guard';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'tableItems', canActivate: [AuthGuardAdmin], component: TableItemsComponent },
+  // {
+  //   path: 'main',
+  //   component: MainComponent,
+  //   children: [
+  //     { path: 'Urls', component: UrlsCompComponent },
+  //     { path: 'Parsers', component: ParsersCompComponent },
+  //     { path: 'Keyword', component: KeywordCompComponent },
+  //     { path: 'NotificationEmail', component: NotificationEmailComponent },
+  //     { path: 'WhiteList', component: WhiteListComponent },
+
+  //   ]
+  // },
+  { path: '**', component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TableItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [AuthGuardAdmin],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
